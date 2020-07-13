8912302b87f11028d1682baa8a1b00526b88a8bb
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-13 19:01:21 -0400

    completed limited guesses

--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,8 @@ import random
 def index(request):
     if 'answer' not in request.session:
         request.session['answer'] = random.randint(1, 100)
-    context = {
-        'answer': request.session['answer'],
-    }
+        request.session['counter'] = 0
+    context = {}
     if 'userNum' in request.session:
         if int(request.session['userNum']) > request.session['answer']:
             context['correct'] = False
@@ -21,15 +20,17 @@ def index(request):
             context['high'] = False
             context['low'] = False
             context['correct'] = True
-    print(context)
+    print(request.session['counter'])
     return render(request, 'index.html', context)
 
 def guess(request):
     userNum = request.POST['guess']
     request.session['userNum'] = userNum
+    request.session['counter'] += 1
     return redirect('/')
 
 def start(request):
     del request.session['answer']
-    del request.session['userNum']
+    if 'userNum' in request.session:
+        del request.session['userNum']
     return redirect('/')
